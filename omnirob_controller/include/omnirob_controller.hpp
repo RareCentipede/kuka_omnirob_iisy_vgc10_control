638722628e30b2d73b4a29e5fc3d69dc8deaca25
Z.lu-5@student.tudelft.nl <stevenlu176@gmail.com>
2026-03-10 13:18:57 +0000

    Implement simple move base service. The loop terminates based on the given speed and estimated time to reach the target. Should make it into an action server later.

--- omnirob_controller/include/omnirob_controller.hpp
+++ omnirob_controller/include/omnirob_controller.hpp
@@ -5,28 +5,32 @@
 #include <memory>
 #include <cmath>
 #include <array>
+#include <eigen3/Eigen/Dense>
 
 #include "geometry_msgs/msg/pose.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "mpnp_interfaces/srv/move_base.hpp"
 #include "rclcpp/rclcpp.hpp"
+
+using Eigen::Vector4d;
 
 class OmnirobController: public rclcpp::Node{
   public:
     OmnirobController();
-    std::array<double, 4> position = {0.0, 0.0, 0.0, 0.0}; // [x, y, z, w]
-    std::array<double, 4> target_position = {0.0, 0.0, 0.0, 0.0}; // [x, y, z, w]
+    Vector4d position; // [x, y, z, w]
+    Vector4d target_position; // [x, y, z, w]
 
   private:
-    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subscription_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_publisher_;
     rclcpp::Service<mpnp_interfaces::srv::MoveBase>::SharedPtr move_base_service_;
 
-    void pose_callback(const geometry_msgs::msg::Pose &pose_msg);
+    void odom_calback(const nav_msgs::msg::Odometry &odom_msg);
     void move_base_service(const std::shared_ptr<mpnp_interfaces::srv::MoveBase::Request> request,
                            std::shared_ptr<mpnp_interfaces::srv::MoveBase::Response> response);
 };
 
-bool isClose(const std::array<double, 4> &home, const std::array<double, 4> &target, double tol);
+bool isClose(const Vector4d &home, const Vector4d &target, double tol);
 
 #endif  // OMNIROB_CONTROLLER_HPP_
--- omnirob_controller/src/omnirob_controller.cpp
+++ omnirob_controller/src/omnirob_controller.cpp
@@ -2,8 +2,8 @@
 
 OmnirobController::OmnirobController() : Node("omnirob_controller") {
     RCLCPP_INFO(this->get_logger(), "hello world omnirob_controller package");
-    subscription_ = this->create_subscription<geometry_msgs::msg::Pose>(
-      "/omnirob/pose", 10, std::bind(&OmnirobController::pose_callback, this, std::placeholders::_1)
+    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/omnirob_controller/odometry", 10, std::bind(&OmnirobController::odom_calback, this, std::placeholders::_1)
     );
 
     twist_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/omnirob_controller/reference", 10);
@@ -12,18 +12,17 @@ OmnirobController::OmnirobController() : Node("omnirob_controller") {
       "/omnirob_controller/move_base", std::bind(&OmnirobController::move_base_service, this,
         std::placeholders::_1, std::placeholders::_2)
     );
+
+    position = {0.0, 0.0, 0.0, 0.0}; // [x, y, z, w]
+    target_position = {0.0, 0.0, 0.0, 0.0}; // [x, y, z, w]
 }
 
-void OmnirobController::pose_callback(const geometry_msgs::msg::Pose &pose_msg) {
-      RCLCPP_INFO(this->get_logger(), "Received pose: position(%f, %f, %f), orientation(%f, %f, %f, %f)",
-      pose_msg.position.x, pose_msg.position.y, pose_msg.position.z,
-      pose_msg.orientation.x, pose_msg.orientation.y, pose_msg.orientation.z, pose_msg.orientation.w);
-
+void OmnirobController::odom_calback(const nav_msgs::msg::Odometry &odom_msg) {
       // Update the current position
-      position[0] = pose_msg.position.x;
-      position[1] = pose_msg.position.y;
-      position[2] = pose_msg.position.z;
-      position[3] = pose_msg.orientation.w; // Assuming we want to track the w component of the orientation as well
+      position[0] = odom_msg.pose.pose.position.x;
+      position[1] = odom_msg.pose.pose.position.y;
+      position[2] = odom_msg.pose.pose.position.z;
+      position[3] = odom_msg.pose.pose.orientation.w; // Assuming we want to track the w component of the orientation as well
 }
 
 void OmnirobController::move_base_service(const std::shared_ptr<mpnp_interfaces::srv::MoveBase::Request> request,
@@ -31,22 +30,50 @@ void OmnirobController::move_base_service(const std::shared_ptr<mpnp_interfaces:
     RCLCPP_INFO(this->get_logger(), "Received move_base request: target_position(%f, %f, %f, %f)",
       request->target_position.x, request->target_position.y, request->target_position.z, request->target_position.w);
 
+    geometry_msgs::msg::TwistStamped twist_msg;
+
     // Update the target position
     target_position[0] = request->target_position.x;
     target_position[1] = request->target_position.y;
     target_position[2] = request->target_position.z;
     target_position[3] = request->target_position.w;
 
-    while (!isClose(position, target_position, 0.5)){
-      ;
+    Vector4d vel = target_position - position;
+    double dist = vel.norm();
+    vel.normalize();
+    vel *= request->speed; // Scale by the requested speed
+    double est_time = dist / request->speed;
+
+    twist_msg.twist.linear.x = vel[0];
+    twist_msg.twist.linear.y = vel[1];
+    twist_msg.twist.linear.z = vel[2];
+    twist_msg.twist.angular.x = 0.0;
+    twist_msg.twist.angular.y = 0.0;
+    twist_msg.twist.angular.z = 0.0;
+
+    double start_time = this->now().seconds();
+    while ((this->now().seconds() - start_time) < est_time){
+      twist_msg.header.stamp = this->now();
+      twist_msg.header.frame_id = "omnirob_base_link"; // Assuming the frame of reference is base
+
+      twist_publisher_->publish(twist_msg);
+      RCLCPP_INFO(this->get_logger(), "Current position: (%f, %f, %f, %f), Target position: (%f, %f, %f, %f)",
+        position[0], position[1], position[2], position[3],
+        target_position[0], target_position[1], target_position[2], target_position[3]);
     }
+
+    // Stop the robot
+    twist_msg.twist.linear.x = 0.0;
+    twist_msg.twist.linear.y = 0.0;
+    twist_msg.twist.linear.z = 0.0;
+    twist_publisher_->publish(twist_msg);
 
     // For now, just respond with success and a message
     response->success = true;
-    response->message = "Target position received successfully";
+    response->message = "Target position reached!";
 }
 
-bool isClose(const std::array<double, 4> &home, const std::array<double, 4> &target, double tol){
+bool isClose(const Vector4d &home, const Vector4d &target, double tol){
   double euclidean_dist = 0.0;
   for (int i = 0; i < 4; i++){
     euclidean_dist += std::pow(home[i] - target[i], 2);
