f0adc78c91b2773879817c20c588ba409f2ffde5
Zhengyang Lu <stevenlu176@gmail.com>
2026-03-10 17:46:24 +0000

    Implement service, had to create temporary node to start MoveGroup. Maybe can find a better way.

--- iisy_arm_controller/src/iisy_arm_controller.cpp
+++ iisy_arm_controller/src/iisy_arm_controller.cpp
@@ -12,19 +12,20 @@ using mpnp_interfaces::srv::MoveArm;
 
 class ArmController: public rclcpp::Node{
   public:
-    ArmController(): Node("iisy_arm_controller") {
-      RCLCPP_INFO(this->get_logger(), "Hello iisy_arm_controller!");
+    ArmController(std::shared_ptr<moveit::planning_interface::MoveGroupInterface> &move_group_interface): Node("iisy_arm_controller") {
+      RCLCPP_INFO(logger, "Hello iisy_arm_controller!");
+      move_arm_service_ = this->create_service<mpnp_interfaces::srv::MoveArm>("iisy/move_arm",
+                        std::bind(&ArmController::move_arm_service, this, std::placeholders::_1, std::placeholders::_2));
+      move_group_interface_ = move_group_interface;
     }
 
-  private:
-    bool success;
-    rclcpp::Logger logger = rclcpp::get_logger("iisy_arm_controller");
+    private:
+    bool success = false;
+    rclcpp::Logger logger = this->get_logger();
     moveit::planning_interface::MoveGroupInterface::Plan plan;
 
-    rclcpp::Service<MoveArm>::SharedPtr move_arm_service_ = this->create_service<mpnp_interfaces::srv::MoveArm>("iisy/move_arm",
-                        std::bind(&ArmController::move_arm_service, this, std::placeholders::_1, std::placeholders::_2));
-    moveit::planning_interface::MoveGroupInterface move_group_interface_ = MoveGroupInterface(shared_from_this(),
-                                                                                              "arm_controller");
+    rclcpp::Service<MoveArm>::SharedPtr move_arm_service_;
+    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface_;
 
     void move_arm_service(const std::shared_ptr<mpnp_interfaces::srv::MoveArm::Request> request,
                           std::shared_ptr<mpnp_interfaces::srv::MoveArm::Response> response){
@@ -36,19 +37,16 @@ class ArmController: public rclcpp::Node{
         target_pose.position.y = target_position.y;
         target_pose.position.z = target_position.z;
         target_pose.orientation.w = target_position.w;
-        target_pose.orientation.x = target_position.x;
-        target_pose.orientation.y = target_position.y;
-        target_pose.orientation.z = target_position.z;
         return target_pose;
       }();
-      move_group_interface_.setPoseTarget(target_pose);
+      move_group_interface_->setPoseTarget(target_pose);
 
-      success = static_cast<bool>(move_group_interface_.plan(plan));
+      success = static_cast<bool>(move_group_interface_->plan(plan));
 
       // Execute the plan if successful
       if (success){
         RCLCPP_INFO(logger, "Plan found, executing...");
-        move_group_interface_.execute(plan);
+        move_group_interface_->execute(plan);
         RCLCPP_INFO(logger, "Motion complete.");
         response->message = "Motion complete.";
       } else {
@@ -63,7 +61,9 @@ class ArmController: public rclcpp::Node{
 int main(int argc, char* argv[]){
   // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
-  auto const node = std::make_shared<ArmController>();
+  auto move_group = std::make_shared<moveit::planning_interface::MoveGroupInterface>(
+    rclcpp::Node::make_shared("temp_node"), "arm_controller");
+  auto const node = std::make_shared<ArmController>(move_group);
   rclcpp::spin(node);
 
   // Shutdown ROS
