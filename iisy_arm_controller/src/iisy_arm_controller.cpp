df292ca8051ad4dfd8dd4d48ccf48b91228546ac
Zhengyang Lu <stevenlu176@gmail.com>
2026-03-10 17:23:51 +0000

    Create arm control interface service.

--- iisy_arm_controller/src/iisy_arm_controller.cpp
+++ iisy_arm_controller/src/iisy_arm_controller.cpp
@@ -1,53 +1,70 @@
 #include <memory>
+#include <cstdio>
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.hpp>
+#include "mpnp_interfaces/srv/move_arm.hpp"
+#include "mpnp_interfaces/msg/vector4.hpp"
+
+using moveit::planning_interface::MoveGroupInterface;
+using mpnp_interfaces::msg::Vector4;
+using mpnp_interfaces::srv::MoveArm;
+
+class ArmController: public rclcpp::Node{
+  public:
+    ArmController(): Node("iisy_arm_controller") {
+      RCLCPP_INFO(this->get_logger(), "Hello iisy_arm_controller!");
+    }
+
+  private:
+    bool success;
+    rclcpp::Logger logger = rclcpp::get_logger("iisy_arm_controller");
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+
+    rclcpp::Service<MoveArm>::SharedPtr move_arm_service_ = this->create_service<mpnp_interfaces::srv::MoveArm>("iisy/move_arm",
+                        std::bind(&ArmController::move_arm_service, this, std::placeholders::_1, std::placeholders::_2));
+    moveit::planning_interface::MoveGroupInterface move_group_interface_ = MoveGroupInterface(shared_from_this(),
+                                                                                              "arm_controller");
+
+    void move_arm_service(const std::shared_ptr<mpnp_interfaces::srv::MoveArm::Request> request,
+                          std::shared_ptr<mpnp_interfaces::srv::MoveArm::Response> response){
+      geometry_msgs::msg::Pose target_pose;
+      Vector4 target_position = request->target_position;
+
+      target_pose = [&target_position, &target_pose]{
+        target_pose.position.x = target_position.x;
+        target_pose.position.y = target_position.y;
+        target_pose.position.z = target_position.z;
+        target_pose.orientation.w = target_position.w;
+        target_pose.orientation.x = target_position.x;
+        target_pose.orientation.y = target_position.y;
+        target_pose.orientation.z = target_position.z;
+        return target_pose;
+      }();
+      move_group_interface_.setPoseTarget(target_pose);
+
+      success = static_cast<bool>(move_group_interface_.plan(plan));
+
+      // Execute the plan if successful
+      if (success){
+        RCLCPP_INFO(logger, "Plan found, executing...");
+        move_group_interface_.execute(plan);
+        RCLCPP_INFO(logger, "Motion complete.");
+        response->message = "Motion complete.";
+      } else {
+        RCLCPP_ERROR(logger, "Planning failed.");
+        response->message = "Planning failed.";
+      }
+
+      response->success = success;
+    }
+};
 
 int main(int argc, char* argv[]){
   // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
-  auto const node = std::make_shared<rclcpp::Node>(
-    "iisy_arm_controller",
-    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
-  );
-
-  // Create a ROS logger
-  auto const logger = rclcpp::get_logger("iisy_arm_controller");
-  RCLCPP_INFO(logger, "Hello iisy_arm_controller!");
-
-  // MoveIt MoveGroup Interface
-  using moveit::planning_interface::MoveGroupInterface;;
-  auto move_group_interface = MoveGroupInterface(node, "arm_controller");
-
-  geometry_msgs::msg::Pose msg;
-  auto const target_pose = [&msg]
-  {
-    msg.orientation.x = 0.964;
-    msg.orientation.y = -0.201;
-    msg.orientation.z = 0.075;
-    msg.orientation.w = 0.159;
-    msg.position.x = 0.133;
-    msg.position.y = 0.05;
-    msg.position.z = 1.216;
-    return msg;
-  }();
-  move_group_interface.setPoseTarget(target_pose);
-
-  // Plan to the target pose
-  auto const [success, plan] = [&move_group_interface]{
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
-
-  // Execute the plan if successful
-  if (success){
-    RCLCPP_INFO(logger, "Plan found, executing...");
-    move_group_interface.execute(plan);
-    RCLCPP_INFO(logger, "Motion complete.");
-  } else {
-    RCLCPP_ERROR(logger, "Planning failed.");
-  }
+  auto const node = std::make_shared<ArmController>();
+  rclcpp::spin(node);
 
   // Shutdown ROS
   rclcpp::shutdown();
