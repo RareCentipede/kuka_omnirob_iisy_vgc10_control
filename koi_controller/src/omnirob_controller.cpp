1c678aced8c291172fee0ee970a3cfd4f39b9ce7
Zhengyang Lu <stevenlu176@gmail.com>
2026-03-24 15:37:09 +0000

    Publish odom static transform. Offset persists.

--- koi_controller/src/omnirob_controller.cpp
+++ koi_controller/src/omnirob_controller.cpp
@@ -25,93 +25,96 @@ OmnirobController::OmnirobController() : Node("omnirob_controller") {
     );
 
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(*this);
+    static_tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(*this);
+
+    auto static_transform = geometry_msgs::msg::TransformStamped();
+    static_transform.header.stamp = this->now();
+    static_transform.header.frame_id = "world";
+    static_transform.child_frame_id = "odom";
+    static_transform.transform.rotation.w = 1.0;
+    static_tf_broadcaster_->sendTransform(static_transform);
 
     position = {0.0, 0.0, 0.0, 0.0}; // [x, y, z, w]
     target_position = {0.0, 0.0, 0.0, 0.0}; // [x, y, z, w]
 }
 
 void OmnirobController::odom_calback(const nav_msgs::msg::Odometry &odom_msg) {
-      // Update the current position
-      position[0] = odom_msg.pose.pose.position.x;
-      position[1] = odom_msg.pose.pose.position.y;
-      position[2] = odom_msg.pose.pose.position.z;
-      position[3] = odom_msg.pose.pose.orientation.w; // Assuming we only care about the yaw component for simplicity
+  // Update the current position
+  position[0] = odom_msg.pose.pose.position.x;
+  position[1] = odom_msg.pose.pose.position.y;
+  position[2] = odom_msg.pose.pose.position.z;
+  position[3] = odom_msg.pose.pose.orientation.w; // Assuming we only care about the yaw component for simplicity
 
-      geometry_msgs::msg::TransformStamped transformStamped;
-      transformStamped.header.stamp = this->now();
-      transformStamped.header.frame_id = "world";
-      transformStamped.child_frame_id = "odom";
-      transformStamped.transform.translation.x = position[0];
-      transformStamped.transform.translation.y = position[1];
-      transformStamped.transform.translation.z = position[2];
-      transformStamped.transform.rotation.w = position[3];
+  geometry_msgs::msg::TransformStamped transformStamped;
+  transformStamped.header.stamp = this->now();
+  transformStamped.header.frame_id = "odom";
+  transformStamped.child_frame_id = "platform_base_link";
+  transformStamped.transform.translation.x = position[0];
+  transformStamped.transform.translation.y = position[1];
+  transformStamped.transform.translation.z = position[2];
+  transformStamped.transform.rotation.w = position[3];
 
-      transformStamped.transform.rotation.x = odom_msg.pose.pose.orientation.x;
-      transformStamped.transform.rotation.y = odom_msg.pose.pose.orientation.y;
-      transformStamped.transform.rotation.z = odom_msg.pose.pose.orientation.z;
+  transformStamped.transform.rotation.x = odom_msg.pose.pose.orientation.x;
+  transformStamped.transform.rotation.y = odom_msg.pose.pose.orientation.y;
+  transformStamped.transform.rotation.z = odom_msg.pose.pose.orientation.z;
 
-      tf_broadcaster_->sendTransform(transformStamped);
-      transformStamped.header.stamp = this->now();
-      transformStamped.child_frame_id = "platform_base_link";
-      transformStamped.transform.translation.x = position[0] - 0.347000;
-      transformStamped.transform.translation.y = position[1] - 0.242000;
-      tf_broadcaster_->sendTransform(transformStamped);
+  tf_broadcaster_->sendTransform(transformStamped);
 }
 
 void OmnirobController::move_base_service(const std::shared_ptr<mpnp_interfaces::srv::MoveBase::Request> request,
                                          std::shared_ptr<mpnp_interfaces::srv::MoveBase::Response> response) {
-    RCLCPP_INFO(this->get_logger(), "Received move_base request: target_position(%f, %f, %f, %f)",
-      request->target_position.x, request->target_position.y, request->target_position.z, request->target_position.w);
+  RCLCPP_INFO(this->get_logger(), "Received move_base request: target_position(%f, %f, %f, %f)",
+    request->target_position.x, request->target_position.y, request->target_position.z, request->target_position.w);
 
-    geometry_msgs::msg::TwistStamped twist_msg;
+  geometry_msgs::msg::TwistStamped twist_msg;
 
-    // Update the target position
-    target_position[0] = request->target_position.x;
-    target_position[1] = request->target_position.y;
-    target_position[2] = request->target_position.z;
-    target_position[3] = request->target_position.w;
+  // Update the target position
+  target_position[0] = request->target_position.x;
+  target_position[1] = request->target_position.y;
+  target_position[2] = request->target_position.z;
+  target_position[3] = request->target_position.w;
 
-    Vector4d vel = target_position - position;
-    double dist = vel.norm();
+  Vector4d vel = target_position - position;
+  double dist = vel.norm();
+  vel.normalize();
+  vel *= request->speed; // Scale by the requested speed
+  double est_time = dist / request->speed;
+
+  double start_time = this->now().seconds();
+  double elapsed_time = this->now().seconds() - start_time;
+  while (!isClose(position, target_position, 1e-3) && (elapsed_time < (est_time * 2.0)))
+  {
+    vel = target_position - position;
     vel.normalize();
     vel *= request->speed; // Scale by the requested speed
-    double est_time = dist / request->speed;
 
-    double start_time = this->now().seconds();
-    double elapsed_time = this->now().seconds() - start_time;
-    while (!isClose(position, target_position, 1e-3) && (elapsed_time < (est_time * 2.0)))
-    {
-      vel = target_position - position;
-      vel.normalize();
-      vel *= request->speed; // Scale by the requested speed
+    twist_msg.header.stamp = this->now();
+    twist_msg.header.frame_id = "platform_base_link"; // Assuming the frame of reference is base
+    twist_msg.twist.linear.x = vel[0];
+    twist_msg.twist.linear.y = vel[1];
+    twist_msg.twist.linear.z = vel[2];
+    twist_msg.twist.angular.x = 0.0;
+    twist_msg.twist.angular.y = 0.0;
+    twist_msg.twist.angular.z = 0.0;
 
-      twist_msg.header.stamp = this->now();
-      twist_msg.header.frame_id = "platform_base_link"; // Assuming the frame of reference is base
-      twist_msg.twist.linear.x = vel[0];
-      twist_msg.twist.linear.y = vel[1];
-      twist_msg.twist.linear.z = vel[2];
-      twist_msg.twist.angular.x = 0.0;
-      twist_msg.twist.angular.y = 0.0;
-      twist_msg.twist.angular.z = 0.0;
-
-      twist_publisher_->publish(twist_msg);
-      elapsed_time = this->now().seconds() - start_time;
-      if ((static_cast<int>(elapsed_time) % 5) < 1e-5){
-        RCLCPP_INFO(this->get_logger(), "Current position: (%f, %f, %f, %f), Target position: (%f, %f, %f, %f)",
-          position[0], position[1], position[2], position[3],
-          target_position[0], target_position[1], target_position[2], target_position[3]);
-      }
-    }
-
-    // Stop the robot
-    twist_msg.twist.linear.x = 0.0;
-    twist_msg.twist.linear.y = 0.0;
-    twist_msg.twist.linear.z = 0.0;
     twist_publisher_->publish(twist_msg);
+    elapsed_time = this->now().seconds() - start_time;
+    if ((static_cast<int>(elapsed_time) % 5) < 1e-5){
+      RCLCPP_INFO(this->get_logger(), "Current position: (%f, %f, %f, %f), Target position: (%f, %f, %f, %f)",
+        position[0], position[1], position[2], position[3],
+        target_position[0], target_position[1], target_position[2], target_position[3]);
+    }
+  }
 
-    // For now, just respond with success and a message
-    response->success = true;
-    response->message = "Target position reached!";
+  // Stop the robot
+  twist_msg.twist.linear.x = 0.0;
+  twist_msg.twist.linear.y = 0.0;
+  twist_msg.twist.linear.z = 0.0;
+  twist_publisher_->publish(twist_msg);
+
+  // For now, just respond with success and a message
+  response->success = true;
+  response->message = "Target position reached!";
 }
 
 bool isClose(const Vector4d &home, const Vector4d &target, double tol){
